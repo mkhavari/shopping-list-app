576d8a64989a1682f2c1f448a691afc6b3fc50bb
mkhavari <mkhavari@consumerfed.org>
2016-04-20 16:50:38 -0400

    project complete

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
 	$("input").keydown(function(e) {
 		if (e.which === 13) {
-			var txt = "<li><span>&#10004</span>	<button id='remove'>X</button></li>";
 			var txt2 = $("input:text").val();
-			$(".list-items").append(txt, txt2);
+			var txt = "<li><span id='check'>&#10004</span> <span id='item'>" + txt2 + "</span> <button id='remove'>X</button></li>";
+			$(".list-items").append(txt);
 			$("input").val('');
 		};
-	})
 	});
 	$("#clear").click(function() {
 		$(".list-items").empty();
 	});
 	$("ul").on('click', '#remove', function(event) {
-		$("#remove").parent().remove();
+		$(this).parent().remove();
 	});
-;	
+	$("ul").on('click', '#check', function(event) {
+		$(this).siblings("#item").css("text-decoration", "line-through");
+		$(this).remove();
+	});
+});	
